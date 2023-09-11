bcfd79e7ede821120639a07b1cf50f1c8d2a6afc
haythambrahem <brahem.haytham@esprit.tn>
2023-09-11 17:08:42 +0100

    firstP

---
+++ src/main/java/tn/esprit/se/springproject00/SpringProject00Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.se.springproject00;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject00Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProject00Application.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/se/springproject00/SpringProject00ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.se.springproject00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject00ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
